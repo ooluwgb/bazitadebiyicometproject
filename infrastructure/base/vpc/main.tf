8bcf9a37264ee7fa247c6b2193c673659893c074
Bazit Adebiyi <adebiyibazit@gmail.com>
2025-07-14 19:56:00 -0500

    testing

--- infrastructure/base/vpc/main.tf
+++ infrastructure/base/vpc/main.tf
@@ -10,7 +10,7 @@ module "vpc" {
     private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
     enable_nat_gateway = true
-    single_nat_gateway = true
+    single_nat_gateway = false
 
     tags = {
         Terraform   = "true"
