748644455a2ffb68241e9b2eb61dbe672b919f8c
Oluwanifemi Oluwagbemila <111203890+ooluwgb@users.noreply.github.com>
2025-07-18 16:35:55 -0500

    Fix Python tests and Dockerfile

--- hello-basit-comet/src/app.py
+++ hello-basit-comet/src/app.py
@@ -5,34 +5,36 @@ app = Flask(__name__)
 POD_NAME = os.getenv("HOSTNAME", "unknown-pod")
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return '''
+    return """
         <form action="/greeting" method="get">
             <label for="name">Hi, what is your name?</label><br><br>
             <input type="text" id="name" name="name" required>
             <input type="submit" value="Submit">
         </form>
-    '''
+    """
 
 
-@app.route('/greeting', methods=['GET'])
+@app.route("/greeting", methods=["GET"])
 def greeting():
-    name = request.args.get('name', 'there') # Default to 'there' if no name is provided
+    name = request.args.get(
+        "name", "there"
+    )  # Default to 'there' if no name is provided
     message = f"Hi {name}, I'm Bazit. This was served from container {POD_NAME}."
 
-    #content negtiation logic
-    if "application/json" in request.headers.get('Accept', ''):
+    # content negtiation logic
+    if "application/json" in request.headers.get("Accept", ""):
         return jsonify({"message": message})
     else:
         return f"<h1>{message}</h1>"
 
 
-@app.route('/healthz', methods=['GET'])
+@app.route("/healthz", methods=["GET"])
 def healthz():
-    #health check endpoint
+    # health check endpoint
     return jsonify({"status": "ok", "pod": POD_NAME})
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
