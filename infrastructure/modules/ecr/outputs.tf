4d523d6e79ea0c9437949e1372c47dedec520123
Basit1854 <adebiyibazit@gmail.com>
2025-07-18 19:57:54 +0000

    fix output

--- infrastructure/modules/ecr/outputs.tf
+++ infrastructure/modules/ecr/outputs.tf
@@ -1,15 +1,4 @@
 output "repository_urls" {
   description = "Map of ECR repository URLs"
-  value = {
-    for repo, details in aws_ecr_repository.this :
-    repo => details.repository_url
-  }
-}
-
-output "repository_arns" {
-  description = "Map of ECR repository ARNs"
-  value = {
-    for repo, details in aws_ecr_repository.this :
-    repo => details.arn
-  }
+  value = module.ecr.repository_urls
 }
